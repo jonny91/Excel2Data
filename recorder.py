77ba6204edcac32eaa0133ea95f6f1bd5949d08c
Jonny <jonny.hong91@gmail.com>
2018-10-30 17:27:02 +0800

    生成目录自动创建

--- recorder.py
+++ recorder.py
@@ -1,10 +1,14 @@
 # -*-coding:utf-8-*-
 __author__ = "Jonny Hong"
 
+import os
 import gzip
 
 
 def record(file_path, content):
+    file_parent = os.path.dirname(file_path)
+    if not os.path.exists(file_parent):
+        os.mkdir(file_parent)
     with open(file_path, "wb+") as f:
         content = gzip.compress(bytes(content, "utf-8"))
         f.write(content)
